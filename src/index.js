d5f9febd0755450d9b6d2d095cfcb9d651f2a330
Yuta Masui <yuta970702m@gmail.com>
2024-05-05 15:02:33 +0900

    [add]初期状態

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Main } from "./main";
+const App = () => {
+	return (
+		<Main />
+	);
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
