e3cdad65ea54d5cf7163056c8ec48164bf4044d4
Yuta Masui <yuta970702m@gmail.com>
2024-05-06 22:07:50 +0900

    [fix]renderの仕方修正

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import { Main } from "./main"
 const App = () => {
 	return (
 		<Main />
 	);
 }
-
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab="home" />);
