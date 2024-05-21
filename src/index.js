6b95f4a1126528b7af132a86a2b17f8cf60c42ec
Yuta Masui <yuta970702m@gmail.com>
2024-05-22 00:00:40 +0900

    preiiter install

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
-import { Main } from "./main"
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Main } from './main'
 const App = () => {
-	return (
-		<Main />
-	);
+    return <Main />
 }
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App tab="home" />);
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App tab="home" />)
